e5423908d46c1915aff0561c3465b2fe9d0de5bf
RoseBB78 <rosemary.ballestero.barquero@ucreativa.com>
2021-05-22 21:40:30 -0600

    New Class created

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,8 +1,11 @@
 package com.ucreativa;
 
+import com.ucreativa.familia.Rosemary;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Rosemary yo = new Rosemary();
+        System.out.println("Hola clase");
     }
 }
---
+++ src/com/ucreativa/familia/Rosemary.java
@@ -0,0 +1,7 @@
+package com.ucreativa.familia;
+
+public class Rosemary {
+    public Rosemary (){
+        System.out.println("Hola soy Rose");
+    }
+}
